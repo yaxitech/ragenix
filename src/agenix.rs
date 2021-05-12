030946db03f58597fb42e474af14c493de136288
Vincent Haupert <mail@vincent-haupert.de>
2021-05-12 20:00:42 +0200

    Eliminate lib

--- src/agenix.rs
+++ src/agenix.rs
@@ -4,7 +4,6 @@ use color_eyre::{
 };
 use jsonschema::JSONSchema;
 use std::{
-    ffi::OsString,
     fs,
     io::Write,
     os::unix::prelude::PermissionsExt,
@@ -12,131 +11,7 @@ use std::{
     process,
 };
 
-use crate::{age, cli, util};
-
-static AGENIX_JSON_SCHEMA: &str = std::include_str!("agenix.schema.json");
-
-#[derive(Debug)]
-struct RagenixRule {
-    pub path: PathBuf,
-    pub public_keys: Vec<String>,
-}
-
-/// Validate conformance of the passed path to the JSON schema [`AGENIX_JSON_SCHEMA`].
-fn validate_rules_file<P: AsRef<Path>>(path: P) -> Result<()> {
-    if !path.as_ref().exists() {
-        return Err(eyre!("{} does not exist!", path.as_ref().to_string_lossy()));
-    }
-
-    let instance = nix_rules_to_json(&path)?;
-    let schema =
-        serde_json::from_str(AGENIX_JSON_SCHEMA).wrap_err("Failed to parse Agenix JSON schema")?;
-    let compiled = JSONSchema::options()
-        .with_draft(jsonschema::Draft::Draft7)
-        .compile(&schema)?;
-    let result = compiled.validate(&instance);
-
-    if let Err(errors) = result {
-        let error_msg = errors
-            .into_iter()
-            .map(|err| format!(" - {}: {}", err.instance_path, err))
-            .collect::<Vec<String>>()
-            .join("\n");
-        Err(eyre!(error_msg))
-    } else {
-        Ok(())
-    }
-}
-
-/// Parse the given rules file path.
-///
-/// This method assumes that the passed file adheres to the [`AGENIX_JSON_SCHEMA`].
-fn parse_rules<P: AsRef<Path>>(rules_path: P) -> Result<Vec<RagenixRule>> {
-    let instance = nix_rules_to_json(&rules_path)?;
-
-    // It's fine to force unwrap here as we validated the JSON schema
-    let mut rules: Vec<RagenixRule> = Vec::new();
-    for (rel_path, val) in instance.as_object().unwrap().iter() {
-        let dir = fs::canonicalize(rules_path.as_ref().parent().unwrap())?;
-        let p = dir.join(&rel_path);
-        let public_keys = val.as_object().unwrap()["publicKeys"]
-            .as_array()
-            .unwrap()
-            .iter()
-            .map(|x| x.as_str().unwrap().to_string())
-            .collect();
-        let rule = RagenixRule {
-            path: p,
-            public_keys,
-        };
-        rules.push(rule);
-    }
-
-    Ok(rules)
-}
-
-/// Rekey all entries with the specified public keys
-fn rekey(entries: &[RagenixRule], identities: &[String], mut writer: impl Write) -> Result<()> {
-    let identities = age::get_identities(identities)?;
-    for entry in entries {
-        if entry.path.exists() {
-            writeln!(writer, "Rekeying {}", entry.path.display())?;
-            age::rekey(&entry.path, &identities, &entry.public_keys)?;
-        } else {
-            writeln!(writer, "Does not exist, ignored: {}", entry.path.display())?;
-        }
-    }
-    Ok(())
-}
-
-/// Edit/create an age-encrypted file
-///
-/// If the file doesn't exist yet, a new file is created and opened in `editor`.
-fn edit(
-    entry: &RagenixRule,
-    identity_paths: &[String],
-    editor: &str,
-    mut writer: impl Write,
-) -> Result<()> {
-    let dir = tempfile::tempdir()?;
-    fs::set_permissions(&dir, PermissionsExt::from_mode(0o700))?;
-
-    let input_path = dir.path().join("input");
-    let output_path = &entry.path;
-
-    if output_path.exists() {
-        // If the file already exists, first decrypt it, hash it, open it in `editor`,
-        // hash the result, and if the hashes are equal, return.
-        let identities = age::get_identities(identity_paths)?;
-        age::decrypt(output_path, &input_path, &identities)?;
-
-        // Calculate hash before editing
-        let pre_edit_hash = util::sha256(&input_path)?;
-
-        // Prompt user to edit file
-        editor_hook(&input_path, &editor)?;
-
-        // Calculate hash after editing
-        let post_edit_hash = util::sha256(&input_path)?;
-
-        // Return if the file wasn't changed when editing
-        if pre_edit_hash == post_edit_hash {
-            writeln!(
-                writer,
-                "{} wasn't changed, skipping re-encryption.",
-                output_path.display()
-            )?;
-            return Ok(());
-        }
-    } else {
-        fs::File::create(&input_path)?;
-        editor_hook(&input_path, &editor)?;
-    }
-
-    age::encrypt(input_path, output_path.clone(), &entry.public_keys)?;
-
-    Ok(())
-}
+use crate::{age, util};
 
 /// Reads the rules file using Nix to output the attribute set as a JSON string.
 /// Return value is parsed into a serde JSON value.
@@ -194,53 +69,130 @@ fn editor_hook(path: &Path, editor: &str) -> Result<()> {
     Ok(())
 }
 
-/// Run the program by parsing the command line arguments and writing
-/// to the passed writer
-#[allow(clippy::missing_errors_doc)]
-#[allow(clippy::missing_panics_doc)]
-pub(crate) fn run<I, T>(itr: I, mut writer: impl Write, mut writer_err: impl Write) -> Result<()>
-where
-    I: IntoIterator<Item = T>,
-    T: Into<OsString> + Clone,
-{
-    let opts = cli::parse_args(itr);
+pub(crate) static AGENIX_JSON_SCHEMA: &str = std::include_str!("agenix.schema.json");
 
-    if opts.schema {
-        write!(writer, "{}", AGENIX_JSON_SCHEMA)?;
+#[derive(Debug)]
+pub(crate) struct RagenixRule {
+    pub path: PathBuf,
+    pub public_keys: Vec<String>,
+}
+
+/// Validate conformance of the passed path to the JSON schema [`AGENIX_JSON_SCHEMA`].
+pub(crate) fn validate_rules_file<P: AsRef<Path>>(path: P) -> Result<()> {
+    if !path.as_ref().exists() {
+        return Err(eyre!("{} does not exist!", path.as_ref().to_string_lossy()));
+    }
+
+    let instance = nix_rules_to_json(&path)?;
+    let schema =
+        serde_json::from_str(AGENIX_JSON_SCHEMA).wrap_err("Failed to parse Agenix JSON schema")?;
+    let compiled = JSONSchema::options()
+        .with_draft(jsonschema::Draft::Draft7)
+        .compile(&schema)?;
+    let result = compiled.validate(&instance);
+
+    if let Err(errors) = result {
+        let error_msg = errors
+            .into_iter()
+            .map(|err| format!(" - {}: {}", err.instance_path, err))
+            .collect::<Vec<String>>()
+            .join("\n");
+        Err(eyre!(error_msg))
     } else {
-        if let Err(report) = validate_rules_file(&opts.rules) {
-            writeln!(
-                writer_err,
-                "error: secrets rules are invalid: '{}'\n{}",
-                &opts.rules, report
-            )?;
-            process::exit(1);
-        }
+        Ok(())
+    }
+}
 
-        let rules = parse_rules(&opts.rules)?;
-        if opts.verbose {
-            writeln!(writer, "{:#?}", rules)?;
-        }
+/// Parse the given rules file path.
+///
+/// This method assumes that the passed file adheres to the [`AGENIX_JSON_SCHEMA`].
+pub(crate) fn parse_rules<P: AsRef<Path>>(rules_path: P) -> Result<Vec<RagenixRule>> {
+    let instance = nix_rules_to_json(&rules_path)?;
 
-        let identities = opts.identities.unwrap_or_default();
+    // It's fine to force unwrap here as we validated the JSON schema
+    let mut rules: Vec<RagenixRule> = Vec::new();
+    for (rel_path, val) in instance.as_object().unwrap().iter() {
+        let dir = fs::canonicalize(rules_path.as_ref().parent().unwrap())?;
+        let p = dir.join(&rel_path);
+        let public_keys = val.as_object().unwrap()["publicKeys"]
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|x| x.as_str().unwrap().to_string())
+            .collect();
+        let rule = RagenixRule {
+            path: p,
+            public_keys,
+        };
+        rules.push(rule);
+    }
 
-        if let Some(path) = &opts.edit {
-            let path_normalized = util::normalize_path(Path::new(path));
-            let edit_path = std::env::current_dir()
-                .and_then(fs::canonicalize)
-                .map(|p| p.join(path_normalized))?;
-            let rule = rules
-                .into_iter()
-                .find(|x| x.path == edit_path)
-                .ok_or_else(|| eyre!("No rule for the given file {}", path))?;
+    Ok(rules)
+}
 
-            // `EDITOR`/`--editor` is mandatory if action is `--edit`
-            let editor = &opts.editor.unwrap();
-            edit(&rule, &identities, editor, &mut writer)?;
-        } else if opts.rekey {
-            rekey(&rules, &identities, &mut writer)?;
+/// Rekey all entries with the specified public keys
+pub(crate) fn rekey(
+    entries: &[RagenixRule],
+    identities: &[String],
+    mut writer: impl Write,
+) -> Result<()> {
+    let identities = age::get_identities(identities)?;
+    for entry in entries {
+        if entry.path.exists() {
+            writeln!(writer, "Rekeying {}", entry.path.display())?;
+            age::rekey(&entry.path, &identities, &entry.public_keys)?;
+        } else {
+            writeln!(writer, "Does not exist, ignored: {}", entry.path.display())?;
         }
     }
+    Ok(())
+}
+
+/// Edit/create an age-encrypted file
+///
+/// If the file doesn't exist yet, a new file is created and opened in `editor`.
+pub(crate) fn edit(
+    entry: &RagenixRule,
+    identity_paths: &[String],
+    editor: &str,
+    mut writer: impl Write,
+) -> Result<()> {
+    let dir = tempfile::tempdir()?;
+    fs::set_permissions(&dir, PermissionsExt::from_mode(0o700))?;
+
+    let input_path = dir.path().join("input");
+    let output_path = &entry.path;
+
+    if output_path.exists() {
+        // If the file already exists, first decrypt it, hash it, open it in `editor`,
+        // hash the result, and if the hashes are equal, return.
+        let identities = age::get_identities(identity_paths)?;
+        age::decrypt(output_path, &input_path, &identities)?;
+
+        // Calculate hash before editing
+        let pre_edit_hash = util::sha256(&input_path)?;
+
+        // Prompt user to edit file
+        editor_hook(&input_path, &editor)?;
+
+        // Calculate hash after editing
+        let post_edit_hash = util::sha256(&input_path)?;
+
+        // Return if the file wasn't changed when editing
+        if pre_edit_hash == post_edit_hash {
+            writeln!(
+                writer,
+                "{} wasn't changed, skipping re-encryption.",
+                output_path.display()
+            )?;
+            return Ok(());
+        }
+    } else {
+        fs::File::create(&input_path)?;
+        editor_hook(&input_path, &editor)?;
+    }
+
+    age::encrypt(input_path, output_path.clone(), &entry.public_keys)?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,63 @@
 use std::env;
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
+use std::{ffi::OsString, fs, io::Write, path::Path, process};
+
+mod age;
+mod agenix;
+mod cli;
+mod util;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    ragenix::agenix(env::args(), &mut std::io::stdout(), &mut std::io::stderr())
+    agenix(env::args(), &mut std::io::stdout(), &mut std::io::stderr())
+}
+
+/// Run the program by parsing the command line arguments and writing
+/// to the passed writer
+pub(crate) fn agenix<I, T>(itr: I, mut writer: impl Write, mut writer_err: impl Write) -> Result<()>
+where
+    I: IntoIterator<Item = T>,
+    T: Into<OsString> + Clone,
+{
+    let opts = cli::parse_args(itr);
+
+    if opts.schema {
+        write!(writer, "{}", agenix::AGENIX_JSON_SCHEMA)?;
+    } else {
+        if let Err(report) = agenix::validate_rules_file(&opts.rules) {
+            writeln!(
+                writer_err,
+                "error: secrets rules are invalid: '{}'\n{}",
+                &opts.rules, report
+            )?;
+            process::exit(1);
+        }
+
+        let rules = agenix::parse_rules(&opts.rules)?;
+        if opts.verbose {
+            writeln!(writer, "{:#?}", rules)?;
+        }
+
+        let identities = opts.identities.unwrap_or_default();
+
+        if let Some(path) = &opts.edit {
+            let path_normalized = util::normalize_path(Path::new(path));
+            let edit_path = std::env::current_dir()
+                .and_then(fs::canonicalize)
+                .map(|p| p.join(path_normalized))?;
+            let rule = rules
+                .into_iter()
+                .find(|x| x.path == edit_path)
+                .ok_or_else(|| eyre!("No rule for the given file {}", path))?;
+
+            // `EDITOR`/`--editor` is mandatory if action is `--edit`
+            let editor = &opts.editor.unwrap();
+            agenix::edit(&rule, &identities, editor, &mut writer)?;
+        } else if opts.rekey {
+            agenix::rekey(&rules, &identities, &mut writer)?;
+        }
+    }
+
+    Ok(())
 }
