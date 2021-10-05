1218c18b22322dcf3c6405aa4e537b50d8c27b11
Vincent Haupert <mail@vincent-haupert.de>
2021-10-06 01:26:45 +0200

    Flake: remove explicit support for `aarch64-darwin` (#30)
    `aarch64-darwin` is now part of `flake-utils.lib.defaultSystems`

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       # Recursively merge a list of attribute sets. Following elements take
       # precedence over previous elements if they have conflicting keys.
       recursiveMerge = with lib; foldl recursiveUpdate { };
-      defaultSystems = flake-utils.lib.defaultSystems ++ [ "aarch64-darwin" ];
+      defaultSystems = flake-utils.lib.defaultSystems;
       eachDefaultSystem = flake-utils.lib.eachSystem (defaultSystems);
       eachLinuxSystem = flake-utils.lib.eachSystem (lib.filter (lib.hasSuffix "-linux") flake-utils.lib.defaultSystems);
 
