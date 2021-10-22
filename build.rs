10f36dc41968379fc0e40fc133b24e2cb82ec729
Vincent Haupert <mail@vincent-haupert.de>
2021-10-22 14:51:52 +0200

    Cargo.lock: update dependencies

--- build.rs
+++ build.rs
@@ -1,26 +1,25 @@
-use std::env;
+use std::{env, io::Error};
 
 use clap_generate::{
+    generate_to,
     generators::{Bash, Fish, Zsh},
-    Generator,
 };
 
 include!("src/cli.rs");
 
-fn generate_completion<G: Generator>(app: &mut App, outdir: &OsString) {
-    clap_generate::generate_to::<G, _, _>(app, crate_name!(), outdir).unwrap();
-}
-
-fn main() {
+fn main() -> Result<(), Error> {
     let outdir = match env::var_os("OUT_DIR") {
-        None => return,
+        None => return Ok(()),
         Some(outdir) => outdir,
     };
 
     let mut app = build();
     app.set_bin_name(crate_name!());
 
-    generate_completion::<Bash>(&mut app, &outdir);
-    generate_completion::<Fish>(&mut app, &outdir);
-    generate_completion::<Zsh>(&mut app, &outdir);
+    let bin_name = crate_name!();
+    generate_to(Bash, &mut app, bin_name, &outdir)?;
+    generate_to(Fish, &mut app, bin_name, &outdir)?;
+    generate_to(Zsh, &mut app, bin_name, &outdir)?;
+
+    Ok(())
 }
