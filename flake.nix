1d9b407b5df2c1a08859eb3298a41ccdaf0af4e7
Bernardo Meurer <bernardo@meurer.org>
2022-01-30 23:49:41 -0800

    refactor(flake): don't use system alias (#90)

--- flake.nix
+++ flake.nix
@@ -352,7 +352,7 @@
 
         # Overlay to add ragenix and replace agenix
         overlay = final: prev: rec {
-          ragenix = self.packages.${prev.system}.ragenix;
+          ragenix = self.packages.${prev.stdenv.hostPlatform.system}.ragenix;
           agenix = ragenix;
         };
       }
