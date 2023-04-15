ff058b9232b1b61536a5e9097d02d83694e2f0cd
Vincent Haupert <vincent@yaxi.tech>
2023-04-15 19:52:01 +0200

    Use crane
    Apart from building the main ragenix package with crane, this commit
    also moves `cargo clippy` and `cargo fmt` to dedicated checks. This
    should avoid build failures in case someone uses a different Rust
    version which might have different policies.

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ rustPlatform
+{ craneLib
 , lib
 , stdenv
 , darwin
@@ -7,79 +7,46 @@
 , nix
 , openssl
 , pkg-config
-, self ? ./.
 , plugins ? [ ]
 }:
 let
-  # Filter out VCS files and files unrelated to the Rust ragenix package
-  filterRustSource = src: with lib; cleanSourceWith {
-    filter = cleanSourceFilter;
-    src = cleanSourceWith {
-      inherit src;
-      filter = name: type:
-        let pathWithoutPrefix = removePrefix (toString src) name; in
-          ! (
-            hasPrefix "/.github" pathWithoutPrefix ||
-            pathWithoutPrefix == "/.gitignore" ||
-            pathWithoutPrefix == "/LICENSE" ||
-            pathWithoutPrefix == "/README.md" ||
-            pathWithoutPrefix == "/docs/ragenix.1.html" ||
-            pathWithoutPrefix == "/docs/ragenix.1.ronn" ||
-            pathWithoutPrefix == "/flake.lock" ||
-            pathWithoutPrefix == "/flake.nix"
-          );
+  commonArgs = {
+    src = lib.cleanSourceWith rec {
+      src = craneLib.path ./.;
+      filter = path: type:
+        let pathWithoutPrefix = lib.removePrefix (toString src) path; in
+        lib.hasPrefix "/docs/" pathWithoutPrefix ||
+        lib.hasPrefix "/example/" pathWithoutPrefix ||
+        lib.hasPrefix "/src/" pathWithoutPrefix ||
+        craneLib.filterCargoSources path type;
     };
+
+    # build dependencies
+    nativeBuildInputs = [
+      pkg-config
+      installShellFiles
+    ] ++ lib.optionals (plugins != [ ]) [
+      makeWrapper
+    ];
+
+    # runtime dependencies
+    buildInputs = [
+      openssl
+    ] ++ lib.optionals stdenv.isDarwin [
+      darwin.Security
+    ];
+
+    # Absolute path to the `nix` binary, used in `build.rs`
+    RAGENIX_NIX_BIN_PATH = lib.getExe nix;
   };
-  rustSource = filterRustSource self;
-  cargoTOML = with builtins; fromTOML (readFile ./Cargo.toml);
+  cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 in
-rustPlatform.buildRustPackage {
-  pname = cargoTOML.package.name;
-  version = cargoTOML.package.version;
-  src = rustSource;
-
-  cargoLock.lockFile = ./Cargo.lock;
-
-  preBuildPhases = [ "codeStyleConformanceCheck" ];
-
-  codeStyleConformanceCheck = ''
-    echo "Checking Rust code formatting"
-    cargo fmt -- --check
-
-    echo "Running clippy"
-    # clippy - use same checkType as check-phase to avoid double building
-    if [ "''${cargoCheckType}" != "debug" ]; then
-        cargoCheckProfileFlag="--''${cargoCheckType}"
-    fi
-    argstr="''${cargoCheckProfileFlag} --workspace --all-features --tests "
-    cargo clippy -j $NIX_BUILD_CORES \
-       $argstr -- \
-       -D clippy::pedantic \
-       -D warnings
-  '';
-
-  # build dependencies
-  nativeBuildInputs = [
-    pkg-config
-    installShellFiles
-  ] ++ lib.optionals (plugins != [ ]) [
-    makeWrapper
-  ];
-
-  # runtime dependencies
-  buildInputs = [
-    openssl
-  ] ++ lib.optionals stdenv.isDarwin [
-    darwin.Security
-  ];
-
-  # Absolute path to the `nix` binary, used in `build.rs`
-  RAGENIX_NIX_BIN_PATH = "${nix}/bin/nix";
+craneLib.buildPackage (commonArgs // {
+  inherit cargoArtifacts;
 
   # Run the tests without the "recursive-nix" feature to allow
   # building the package without having a recursive-nix-enabled Nix.
-  checkNoDefaultFeatures = true;
-  doCheck = true;
+  cargoTestExtraArgs = "--no-default-features";
 
   postInstall = ''
     set -euo pipefail
@@ -88,12 +55,13 @@ rustPlatform.buildRustPackage {
     ln -sr "$out/bin/ragenix" "$out/bin/agenix"
 
     # Stdout of build.rs
-    buildOut=$(find "$tmpDir/build" -type f -regex ".*\/ragenix-[a-z0-9]+\/output")
+    buildOut=$(grep -m 1 -Rl 'RAGENIX_COMPLETIONS_BASH=/' target/ | head -n 1)
+    printf "found build script output at %s\n" "$buildOut"
 
     set +u # required due to `installShellCompletion`'s implementation
-    installShellCompletion --bash "$(grep -oP 'RAGENIX_COMPLETIONS_BASH=\K.*' $buildOut)"
-    installShellCompletion --zsh  "$(grep -oP 'RAGENIX_COMPLETIONS_ZSH=\K.*' $buildOut)"
-    installShellCompletion --fish "$(grep -oP 'RAGENIX_COMPLETIONS_FISH=\K.*' $buildOut)"
+    installShellCompletion --bash "$(grep -oP 'RAGENIX_COMPLETIONS_BASH=\K.+' "$buildOut")"
+    installShellCompletion --zsh  "$(grep -oP 'RAGENIX_COMPLETIONS_ZSH=\K.+'  "$buildOut")"
+    installShellCompletion --fish "$(grep -oP 'RAGENIX_COMPLETIONS_FISH=\K.+' "$buildOut")"
 
     installManPage docs/ragenix.1
   '';
@@ -102,4 +70,6 @@ rustPlatform.buildRustPackage {
   postFixup = lib.optionalString (plugins != [ ]) ''
     wrapProgram "$out/bin/ragenix" --suffix PATH : ${lib.strings.makeBinPath plugins}
   '';
-}
+
+  passthru.cargoArtifacts = cargoArtifacts;
+})
